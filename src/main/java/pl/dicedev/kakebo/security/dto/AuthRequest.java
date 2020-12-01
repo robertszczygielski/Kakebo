45af2ff0196032cb8c7c5c823930d85a310c1ed6
Robert Szczygielski <robert.szczygielski@wp.pl>
2020-12-01 22:10:37 +0100

    feat(Backend): Basic security
    Add JWT as a security token

---
+++ src/main/java/pl/dicedev/kakebo/security/dto/AuthRequest.java
@@ -0,0 +1,17 @@
+package pl.dicedev.kakebo.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class AuthRequest {
+
+    private String username;
+    private String password;
+
+}
