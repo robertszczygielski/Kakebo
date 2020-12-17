b40169edf6076330debbf0a43d36d519dfd1dd66
Robert Szczygielski <robert.szczygielski@wp.pl>
2020-12-17 19:56:47 +0100

    feat(Frontend): send user to backend

--- src/front/src/register/api/RegisterApi.tsx
+++ src/front/src/register/api/RegisterApi.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { AUTH_URI } from "../../api/main";
 
-export const registerUser: any = () => {
-    return axios.post(AUTH_URI + "createUser", {"username": "admin@admin.com", "password": "admin"})
+export const registerUser: any = (username: string, password: string) => {
+    return axios.post(AUTH_URI + "createUser", {"username": username, "password": password})
         .then((rest) => { return rest.data })
         .catch(err => console.error(err))
 }
