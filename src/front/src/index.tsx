a07547ea07a59a4ccf9a4f5eabe84a1e433f4f58
Robert Szczygielski <robert.szczygielski@wp.pl>
2020-12-24 18:49:49 +0100

    feat(Frontend): Routing
    Bootstrap

--- src/front/src/index.tsx
+++ src/front/src/index.tsx
@@ -3,48 +3,47 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import setAuthenticationToken from "./api/main";
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { LoginBasic } from "./login/Login";
 import { AssetForm } from "./assets/components/AssetForm";
 import { RegisterBasic } from "./register/Register";
 import App from "./App";
+import { Nav } from "react-bootstrap";
 
 setAuthenticationToken(localStorage.jwtToken);
 
 ReactDOM.render(
     <Router>
         <div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">Login</Link>
-                    </li>
-                    <li>
-                        <Link to="/allAsset">All Assets</Link>
-                    </li>
-                    <li>
-                        <Link to="/addAsset">Add Assets</Link>
-                    </li>
-                </ul>
-            </nav>
+            <Nav variant="tabs" defaultActiveKey="/">
+                <Nav.Item>
+                    <Nav.Link href="/">Home</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="/login">Login</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="/allAsset">All Assets</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="/addAsset">Add Assets</Nav.Link>
+                </Nav.Item>
+            </Nav>
 
             {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
             <Switch>
                 <Route path="/login">
-                    <LoginBasic />
+                    <LoginBasic/>
                 </Route>
                 <Route path="/allAsset">
-                    <App />
+                    <App/>
                 </Route>
                 <Route path="/addAsset">
-                    <AssetForm />
+                    <AssetForm/>
                 </Route>
                 <Route path="/">
-                    <RegisterBasic />
+                    <RegisterBasic/>
                 </Route>
             </Switch>
         </div>
