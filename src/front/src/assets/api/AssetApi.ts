4c9881b2cd0cde4b617164bae70352cd9d2465b2
Robert Szczygielski <robert.szczygielski@wp.pl>
2021-01-20 21:17:39 +0100

    feature(Back): Update asset end point

--- src/front/src/assets/api/AssetApi.ts
+++ src/front/src/assets/api/AssetApi.ts
@@ -2,13 +2,13 @@ import axios from "axios"
 import { ASSETS_URI } from "../../api/main";
 
 export const getAllAssets: any = () => {
-    return axios.get(ASSETS_URI + "get")
+    return axios.get(ASSETS_URI)
         .then((rest) => { return rest.data })
         .catch(err => console.error(err))
 }
 
 export const setAsset: any = (value: number) => {
-    return axios.post(ASSETS_URI + "set", {"amount": value})
+    return axios.post(ASSETS_URI, {"amount": value})
         .then((rest) => { return rest.data })
         .catch(err => console.error(err))
 }
