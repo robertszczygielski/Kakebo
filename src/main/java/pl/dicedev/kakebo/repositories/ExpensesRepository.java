09812435b508737909138e222e87c9a2e3b5685e
Robert Szczygielski <robert.szczygielski@wp.pl>
2021-01-23 18:04:56 +0100

    feature(Back): Find Expense by category in repository

--- src/main/java/pl/dicedev/kakebo/repositories/ExpensesRepository.java
+++ src/main/java/pl/dicedev/kakebo/repositories/ExpensesRepository.java
@@ -3,15 +3,19 @@ package pl.dicedev.kakebo.repositories;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import pl.dicedev.kakebo.enums.ExpensesCategory;
 import pl.dicedev.kakebo.repositories.entities.ExpensesEntity;
 import pl.dicedev.kakebo.repositories.entities.UserEntity;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ExpensesRepository extends CrudRepository<ExpensesEntity, UUID> {
 
     @Query("select sum(e.amount) from ExpensesEntity e where e.user = :user")
     BigDecimal countAllExpenses(@Param("user") UserEntity user);
+
+    Optional<ExpensesEntity> findAllByUserAndExpensesCategory(UserEntity user, ExpensesCategory expensesCategory);
 
 }
