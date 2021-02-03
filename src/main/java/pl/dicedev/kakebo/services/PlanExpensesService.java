5bfe03a500d62692f530fff55200a96d00fa4a90
Robert Szczygielski <robert.szczygielski@wp.pl>
2021-02-03 20:12:56 +0100

    feature(Back): Add plan expenses interfaces service

---
+++ src/main/java/pl/dicedev/kakebo/services/PlanExpensesService.java
@@ -0,0 +1,9 @@
+package pl.dicedev.kakebo.services;
+
+import pl.dicedev.kakebo.services.dtos.PlanExpensesDto;
+
+public interface PlanExpensesService {
+
+    public void addPlanExpenses(PlanExpensesDto planExpensesDto);
+
+}
