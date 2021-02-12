e9b1decc80bf0588f0af2df13822bba1fe0e700e
Robert Szczygielski <robert.szczygielski@wp.pl>
2021-02-12 19:54:39 +0100

    feature(Front): Show dropbox with asset category

--- src/front/src/assets/components/AssetForm.tsx
+++ src/front/src/assets/components/AssetForm.tsx
@@ -7,13 +7,10 @@ interface IAsset {
     asset: number;
 }
 
-interface IAssetCategory {
-    id: string;
-    category: string;
-}
-
 export const AssetForm: React.FC = () => {
-    const [assetsCategories, setAssetsCategories] = useState<Array<IAssetCategory | null> | null>([]);
+    const [assetsCategories, setAssetsCategories] = useState<Array<string>>(
+        []
+    );
 
     const initialValues: IAsset = {
         asset: 0,
@@ -26,9 +23,8 @@ export const AssetForm: React.FC = () => {
 
     const findAllAssetsCategories = () => {
         getAllAssetsCategories().then((data: any) => {
-            const assetsCategory: Array<IAssetCategory | null> = data;
-            console.log(assetsCategory);
-            setAssetsCategories(assetsCategory);
+            const assetsCategory: Array<string> = data;
+            setAssetsCategories([...assetsCategory]);
         })
     }
 
@@ -45,6 +41,11 @@ export const AssetForm: React.FC = () => {
             >
                 <Form>
                     <Field type="text" id="asset" name="asset" placeholder="0"/>
+                    <Field as="select" id="assetCategory">
+                        {assetsCategories?.map(assetsCategory =>(
+                            <option value={assetsCategory}>{assetsCategory}</option>
+                        ))}
+                    </Field>
                     <button type="submit">Submit</button>
                 </Form>
             </Formik>
