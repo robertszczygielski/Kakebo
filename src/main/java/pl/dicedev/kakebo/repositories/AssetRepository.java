bdcf6e9ae8d1392925362dd3e25fa1f603d5142e
Robert Szczygielski <robert.szczygielski@wp.pl>
2021-02-25 18:52:24 +0100

    feature(Backend): add find by category in to repository

--- src/main/java/pl/dicedev/kakebo/repositories/AssetRepository.java
+++ src/main/java/pl/dicedev/kakebo/repositories/AssetRepository.java
@@ -2,10 +2,15 @@ package pl.dicedev.kakebo.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import pl.dicedev.kakebo.enums.AssetCategory;
 import pl.dicedev.kakebo.repositories.entities.AssetEntity;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface AssetRepository extends CrudRepository<AssetEntity, UUID> {
+
+    List<AssetEntity> getAssetEntitiesByAssetCategory(AssetCategory assetCategory);
+
 }
