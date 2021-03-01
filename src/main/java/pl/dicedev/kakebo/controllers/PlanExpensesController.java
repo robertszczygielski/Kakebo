4ae0d724107ede46df91754e70fbe1af9855abdb
Robert Szczygielski <robert.szczygielski@wp.pl>
2021-03-01 22:30:59 +0100

    feature(Backend): add to plane expenses controller - srvice

--- src/main/java/pl/dicedev/kakebo/controllers/PlanExpensesController.java
+++ src/main/java/pl/dicedev/kakebo/controllers/PlanExpensesController.java
@@ -1,17 +1,22 @@
 package pl.dicedev.kakebo.controllers;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.dicedev.kakebo.services.PlanExpensesService;
 import pl.dicedev.kakebo.services.dtos.PlanExpensesDto;
 
 @RestController
 @RequestMapping("/plan/expenses")
+@AllArgsConstructor
 public class PlanExpensesController {
+
+    private final PlanExpensesService planExpensesService;
 
     @PostMapping
     public void addPlanExpenses(PlanExpensesDto planExpensesDto) {
-
+        planExpensesService.addPlanExpenses(planExpensesDto);
     }
 
 }
