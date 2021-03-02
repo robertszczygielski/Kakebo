9d5fb9aa37040b8e2ab856de40136841e15cd10c
Robert Szczygielski <robert.szczygielski@wp.pl>
2021-03-02 17:45:59 +0100

    feature(Backend): add to plane expenses srvice method who is getting all plans form database

--- src/main/java/pl/dicedev/kakebo/services/PlanExpensesService.java
+++ src/main/java/pl/dicedev/kakebo/services/PlanExpensesService.java
@@ -2,8 +2,12 @@ package pl.dicedev.kakebo.services;
 
 import pl.dicedev.kakebo.services.dtos.PlanExpensesDto;
 
+import java.util.List;
+
 public interface PlanExpensesService {
 
     void addPlanExpenses(PlanExpensesDto planExpensesDto);
+
+    List<PlanExpensesDto>  getPlanExpenses();
 
 }
