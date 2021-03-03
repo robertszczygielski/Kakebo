5a5c31a88c5cdf6f8838ea68fac745f398f086f6
Robert Szczygielski <robert.szczygielski@wp.pl>
2021-03-03 20:34:26 +0100

    feature(Backend): get plan expenses form database and map to dto

--- src/main/java/pl/dicedev/kakebo/repositories/PlanExpensesRepository.java
+++ src/main/java/pl/dicedev/kakebo/repositories/PlanExpensesRepository.java
@@ -1,9 +1,9 @@
 package pl.dicedev.kakebo.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import pl.dicedev.kakebo.repositories.entities.PlanExpensesEntity;
 
 import java.util.UUID;
 
-public interface PlanExpensesRepository extends CrudRepository<PlanExpensesEntity, UUID> {
+public interface PlanExpensesRepository extends JpaRepository<PlanExpensesEntity, UUID> {
 }
