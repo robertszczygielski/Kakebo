f1a56b900b731d2554df6b24faeaa6f68242431a
Robert Szczygielski <robert.szczygielski@wp.pl>
2021-03-11 19:38:33 +0100

    feature(Frontend): Update User Endpoint

--- src/front/src/auth/register/api/RegisterApi.tsx
+++ src/front/src/auth/register/api/RegisterApi.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { AUTH_URI } from "../../../api/main";
 
 export const registerUser: any = (username: string, password: string) => {
-    return axios.post(AUTH_URI + "createUser", {"username": username, "password": password})
+    return axios.post(AUTH_URI + "user/create", {"username": username, "password": password})
         .then((rest) => { return rest.data })
         .catch(err => console.error(err))
 }
