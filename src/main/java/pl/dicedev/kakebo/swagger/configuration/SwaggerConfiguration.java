a32ff593d87822a919d4c4e652118df1426c709c
Robert Szczygielski <robert.szczygielski@wp.pl>
2021-04-15 19:34:47 +0200

    Feature (Backend): Add title for swagger config

--- src/main/java/pl/dicedev/kakebo/swagger/configuration/SwaggerConfiguration.java
+++ src/main/java/pl/dicedev/kakebo/swagger/configuration/SwaggerConfiguration.java
@@ -2,12 +2,10 @@ package pl.dicedev.kakebo.swagger.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
-import springfox.documentation.service.SecurityScheme;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -28,7 +26,14 @@ public class SwaggerConfiguration {
                 .paths(PathSelectors.any())
                 .build()
                 .securitySchemes(List.of(createSchema()))
-                .securityContexts(List.of(createContext()));
+                .securityContexts(List.of(createContext()))
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Kakebo Budget Application")
+                .build();
     }
 
     private SecurityScheme createSchema() {
