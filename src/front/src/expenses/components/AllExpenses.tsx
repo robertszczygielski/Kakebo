4108097954752d2d4f96e3e86e112250fe2412d5
Robert Szczygielski <robert.szczygielski@wp.pl>
2021-04-21 19:14:45 +0200

    Feature (Frontend): Add all expenses component

---
+++ src/front/src/expenses/components/AllExpenses.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { getAllExpenses } from "../api/ExpensesApi";
+
+interface IExpenses {
+    id: string;
+    amount: number;
+    expensesDate: string;
+    expensesCategory: string
+}
+
+const AllExpenses: React.FC = () => {
+    const [expenses, setExpenses] = useState<Array<IExpenses | null> | null>([]);
+
+    useEffect(() => {
+        findAllExpenses();
+    }, [])
+
+    const findAllExpenses = () => {
+        getAllExpenses().then((data: any) => {
+            const assets: Array<IExpenses | null> = data;
+            setExpenses(assets);
+        })
+    }
+
+    return (
+        <div>
+            <ul>
+                {expenses?.map(asset => (
+                    <li key={asset?.id}>{asset?.amount} - {asset?.expensesCategory} - {asset?.expensesDate}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default AllExpenses;
