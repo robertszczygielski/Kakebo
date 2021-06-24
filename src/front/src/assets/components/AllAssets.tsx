0e068c73393995265aa962e9a8a87473e41cdad0
Robert Szczygielski <robert.szczygielski@wp.pl>
2021-06-24 21:31:45 +0200

    Feature (Frontend): Add React List

--- src/front/src/assets/components/AllAssets.tsx
+++ src/front/src/assets/components/AllAssets.tsx
@@ -15,16 +15,16 @@ const AllAssets: React.FC = () => {
     const data = React.useMemo(
         () => [
             {
-                col1: 'Hello',
-                col2: 'World',
+                amount: 'Hello',
+                assetCategory: 'World',
             },
             {
-                col1: 'react-table',
-                col2: 'rocks',
+                amount: 'react-table',
+                assetCategory: 'rocks',
             },
             {
-                col1: 'whatever',
-                col2: 'you want',
+                amount: 'whatever',
+                assetCategory: 'you want',
             },
         ],
         []
